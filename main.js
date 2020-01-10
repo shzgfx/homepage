5d472ea8834c744117f2f0d0630311b910a8fd98
Tomasz Kępa <tomaszkepa10@gmail.com>
2020-01-10 21:33:01 +0100

    FEAT: added flex to nav and new js script

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert("siema");
+
+let firstName = prompt("Wpisz swoje imie: ");
+let lastName = prompt("wpisz swoje nazwisko: ");
+
+function hello(firstName, lastName) {
+    console.log(`Hej ${firstName} ${lastName}! Witam Cię na mojej stronie:)`)
+}
+
+hello(firstName, lastName);
